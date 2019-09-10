a006077bb84111756e6be4b2368add3e16e63997
Dominick Tournour <dominick.tournour@libertyits.com>
2019-09-10 16:17:54 -0400

    initial commit

---
+++ build.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+
+#
+# Disable debugging unless explicitly set
+#
+set +x
+if [ -z "$DEBUG" ]; then DEBUG=false; fi
+export DEBUG
+if [ "${DEBUG}" == true ]; then
+  set -x
+  env | sort
+fi
+
+
+#
+# Ensure that we fail fast on any issues.
+#
+set -euo pipefail
+
+#
+# Make our utilities available on the path
+#
+#export PATH=$WORKSPACE/bin:$PATH
+
+doUpgrade() {
+  dockerRepo=vasdvp/health-apis-spring-boot-application-base
+  tag=$1
+  sectag=$1-sec-scan
+  
+  #Build the new spring-boot-application-base
+  doDockerBuild
+  
+  #Push the sectag to DockerHub
+  doDockerPush $sectag
+  
+  #give the image 30 sec before we pull it down to use it.
+  sleep 30
+  
+  #Build the Test Application for the specific version
+  buildTestApplication
+
+  #Test the application that was just launched.  For a specific version
+  testApplication
+
+  #Tag the sectag as the regular tag
+  retagDockerImage
+
+  #Push to repo with real tag
+  #doDockerPush $tag
+}
+
+doDockerBuild(){
+  #Build local docker image with the custom tag
+  docker build -f Dockerfile$VERSION -t $dockerRepo:$sectag .
+}
+
+doDockerPush(){
+  #Push docker image
+  docker push vasdvp/health-apis-spring-boot-application-base:$1
+}
+
+buildTestApplication(){
+  #going to need to figure out how to do this.  Probably want to pull down an easy repo (IDS for jdk-12?). 
+  #mvn clean install -Ddocker.baseImage=$dockerRepo -Ddocker.baseVersion=$sectag -Prelease
+  #this will create a local docker image.  WIll need to figure out how to launch it standalone to do any kind of testing....]
+  
+  echo $tag
+}
+
+testApplication(){
+  #will require different applications per spring-boot-application-base (8 and 12?)?
+  #Test the local docker image.  Running some kind of deploy-local.sh and test-local.sh
+  #if it passes then we are good
+  echo $tag
+}
+
+retagDockerImage(){
+  docker tag $dockerRepo:$sectag $dockerRepo:$tag
+}
+
+
+
+[ $# == 0 ] && echo "No jdk version specified. Supported versions are 8 and 12." && exit 1
+VERSION=$1
+
+case "$VERSION" in
+  8) doUpgrade jdk-8 ;;
+  12) doUpgrade jdk-12 ;;
+  *) echo "Unknown version: $VERSION. Supported versions are 8 and 12." && exit 1 ;;
+esac
---
+++ entrypoint.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+#
+# Get files off s3 bucket
+#
+[ -z "$AWS_CONFIG_FOLDER" ] && AWS_CONFIG_FOLDER=$AWS_APP_NAME
+aws s3 cp s3://$AWS_BUCKET_NAME/$AWS_CONFIG_FOLDER/ /opt/va/ --recursive
+aws s3 cp s3://$AWS_BUCKET_NAME/system_certs/ /opt/va/certs --recursive
+aws s3 cp s3://$AWS_BUCKET_NAME/krb5/krb5.conf /etc
+
+
+#
+# Start up app and log activity
+# If a start up hook exeists, execute it
+#
+cd /opt/va/
+
+HOOK=on-start.sh
+if [ -f $HOOK ]
+then
+  echo ============================================================
+  echo "Running start up HOOK"
+  chmod +x $HOOK
+  ./$HOOK
+  HOOK_STATUS=$?
+  [ $HOOK_STATUS != 0 ] && echo "Start up hook failed with status $HOOK_STATUS" && exit 1
+fi
+
+echo ============================================================
+java -jar $(find .  -type f -name "$AWS_APP_NAME-*.jar" | head -1)
