edbfe329f8c8b33334bcd3173f81d4779a409ac3
Dominick Tournour <dominick.tournour@libertyits.com>
2019-10-25 15:04:01 -0400

    build.sh

--- build.sh
+++ build.sh
@@ -8,14 +8,13 @@ set -euo pipefail
 
 doUpgrade() {
   dockerRepo=vasdvp/health-apis-spring-boot-application-base
-  tag=jdk-$1
-  sectag=$1-sec-scan
+  sectag=jdk-$1-rc
 
   docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
   docker build -f Dockerfile$1 --build-arg CACHBREAKER=$(date +%D)_spring_boot_application_base -t $1:$2 .
   docker push $1:$2
 }
 
-doUpgrade 8 ;;
-doUpgrade 12 ;;
+doUpgrade 8
+doUpgrade 12
 
