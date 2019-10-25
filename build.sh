280568d3a7f6be6f638c7d53a5d6173a2ae1339a
Dominick Tournour <dominick.tournour@libertyits.com>
2019-10-25 16:36:00 -0400

    fixed shell script

--- build.sh
+++ build.sh
@@ -9,10 +9,12 @@ set -euo pipefail
 doUpgrade() {
   dockerRepo=vasdvp/health-apis-spring-boot-application-base
   sectag=jdk-$1-rc
+  date=$(date +%D)
 
-  docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-  docker build -f Dockerfile$1 --build-arg CACHBREAKER=$(date +%D)_spring_boot_application_base -t $1:$2 .
-  docker push $1:$2
+  echo $date
+  #docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+  docker build -f Dockerfile$1 --build-arg CACHBREAKER=$date-spring_boot_application_base -t $dockerRepo:$sectag .
+  docker push $dockerRepo:$sectag
 }
 
 doUpgrade 8
