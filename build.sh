a5309baefa899e192bb150167b254a3a158e8a6d
Dominick Tournour <dominick.tournour@libertyits.com>
2019-10-25 15:00:37 -0400

    build both 8 and 12

--- build.sh
+++ build.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-#
-# Disable debugging unless explicitly set
-#
-set +x
-if [ -z "$DEBUG" ]; then DEBUG=false; fi
-export DEBUG
-if [ "${DEBUG}" == true ]; then
-  set -x
-  env | sort
-fi
 
 #
 # Ensure that we fail fast on any issues.
@@ -18,42 +8,14 @@ set -euo pipefail
 
 doUpgrade() {
   dockerRepo=vasdvp/health-apis-spring-boot-application-base
-  tag=$1
+  tag=jdk-$1
   sectag=$1-sec-scan
 
   docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-  
-  #Build the new spring-boot-application-base
-  doDockerBuild $dockerRepo $sectag
-  
-  #Push the sectag to DockerHub
-  doDockerPush $dockerRepo $sectag
-}
-
-doDockerBuild(){
-  #Build local docker image with the custom tag
-  docker build -f Dockerfile$VERSION --build-arg CACHBREAKER=$(date +%D)_spring_boot_application_base -t $1:$2 .
-}
-
-doDockerPush(){
-  #Push docker image
+  docker build -f Dockerfile$1 --build-arg CACHBREAKER=$(date +%D)_spring_boot_application_base -t $1:$2 .
   docker push $1:$2
 }
 
-if [ "$APPLICATION_BASE_VERSION" == "none" ]
-then
-  echo "Spring Boot Appication Base Upgrade"
-  echo "Nothing Built"
-  echo "Nothing Pushed"
-  echo "This job really just did nothing"
-  echo "\" I'm just trying to do as little as possible \" - My Hero"
-  exit 0
-else
-  VERSION=$APPLICATION_BASE_VERSION
-fi 
+doUpgrade 8 ;;
+doUpgrade 12 ;;
 
-case "$VERSION" in
-  8) doUpgrade jdk-8 ;;
-  12) doUpgrade jdk-12 ;;
-  *) echo "Unknown version: $VERSION. Supported versions are 8 and 12." && exit 1 ;;
-esac
